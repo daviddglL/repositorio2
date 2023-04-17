f9b9e12f20c0a021c1451dfe8ca35b783bfe3fd1
david <delgadogarciadelomasdavid@gmail.com>
2023-04-17 13:17:59 +0200

    Incidencia 1. Nuevo desarrollo

---
+++ entrada.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class entrada {
+    public String path;
+}
--- main.java
+++ main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Recursividad recursivo =new Recursividad();
         int n = 10;
+        
         System.out.println("El número de fibonacci de "+n+" es: "+recursivo.conejos(n));
     }
 }
